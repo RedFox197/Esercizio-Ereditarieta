7a17ec1576236c34f2ceae1f198db243e909178e
Vincenzo Colluto <86895240+vcolluto@users.noreply.github.com>
2025-01-22 09:58:02 +0100

    classe Persona

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+import org.generation.italy.jaita138.oop.model.Persona;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Persona p=new Persona("Mario", "Rossi", LocalDate.of(1980, 4, 12));
+        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        p.saluta();
+        
+
+        Scanner sc=new Scanner(System.in);
+       
+       
+        Persona p1=null;
+        do {
+            System.out.print("Inserisci il nome: ");
+            String nome=sc.nextLine();   
+            System.out.print("Inserisci il cognome: ");
+            String cognome=sc.nextLine();   
+            System.out.print("Inserisci la data di nascita: ");
+                  
+            try {
+                LocalDate dn=LocalDate.parse(sc.nextLine(),df);  
+                p1=new Persona(nome, cognome, dn);            
+            } catch (DateTimeParseException e) {
+                System.out.println("Impossibile creare la persona: Data non valida");
+            } catch (Exception e) {
+                System.out.println("Impossibile creare la persona: "+e.getMessage());
+            }
+        }  while(p1==null);;
+        p1.saluta();
+
     }
 }
---
+++ src/org/generation/italy/jaita138/oop/model/Persona.java
@@ -0,0 +1,55 @@
+package org.generation.italy.jaita138.oop.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Persona {
+    private String nome,cognome;
+    private LocalDate dataNascita;
+   
+    public Persona(String nome, String cognome, LocalDate dataNascita) {
+        setNome(nome);       
+        setCognome(cognome); 
+        
+        if (dataNascita.isBefore(LocalDate.now()))
+            this.dataNascita = dataNascita;
+        else
+            throw new IllegalArgumentException("Data di nascita non valida"); 
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (!nome.trim().isEmpty())
+            this.nome = nome;
+        else
+            throw new IllegalArgumentException("Nome non valido");  //se il nome è vuoto genero un'eccezione
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        if (!cognome.trim().isEmpty())
+            this.cognome = cognome;
+        else
+            throw new IllegalArgumentException("Cognome non valido");  //se il nome è vuoto genero un'eccezione
+    }
+
+    public void saluta() {
+        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.printf("Ciao mi chiamo %s %s e sono nato il %s%n",nome,cognome,dataNascita.format(df));
+    }
+
+    
+
+    
+    
+}
